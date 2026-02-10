0e6841688d957dc99020c0d6949d678deb80dcf5
Qadir296 <44345295+Qadir296@users.noreply.github.com>
2026-02-10 11:03:39 +0000

    Deploying to gh-pages from @ Qadir296/qadir296.github.io@ba2e3976e91851b19ccca66e11997ac0e0816bff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -470,13 +470,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -503,13 +496,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=ieIkq6wAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
       id: 'light-theme',
